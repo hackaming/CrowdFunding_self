f9ac4843a4f79b74a9f53c04b74cbd9d5705ddae
hackaming <2251275876@qq.com>
2017-08-14 11:23:36 +0800

    audit 的dao,service写完了，后面开始写页面，逻辑，控制层了。COMMIT一下，下午练车去。

---
+++ Crowdfunding/src/main/java/com/crowdfunding/sjtu/service/IAuditService.java
@@ -0,0 +1,13 @@
+package com.crowdfunding.sjtu.service;
+
+import java.util.List;
+
+import com.crowdfunding.sjtu.model.Audit;
+
+public interface IAuditService {
+	public List<Audit> getAudits();
+	public void deleteAuditItem(Audit audit);
+	public void saveAudit(Audit audit);
+	public void SaveOrUpdateAudit(Audit audit);
+	public Audit findById(int auditId);
+}
