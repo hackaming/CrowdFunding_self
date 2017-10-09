1aa2fc1b85ca47692ac152e8115f6df31ff72ede
hackaming <2251275876@qq.com>
2017-10-09 13:40:21 +0800

    now the message can be listened and can be send out. next step's to get the cpu usage, get a server list, and RPC to save it in to db. late need to add the redis support.

--- Crowdfunding/src/main/java/com/crowdfunding/sjtu/AQM/ReadQueueAndDistribute.java
+++ Crowdfunding/src/main/java/com/crowdfunding/sjtu/AQM/ReadQueueAndDistribute.java
@@ -9,6 +9,7 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -16,58 +17,26 @@ import org.springframework.stereotype.Service;
 
 import com.crowdfunding.sjtu.Vo.RequestSerialVO;
 
-
 public class ReadQueueAndDistribute implements MessageListener {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
     
 	@Override
 	public void onMessage(Message message) {
-		System.out.println("test*************test");
-		System.out.println(message.toString());
-		 byte[] dd  = message.getBody();
-		System.out.println("test*************test");
-		System.out.println(dd);
-		message.getBody();
-		System.out.println(message.getBody().toString());
+		Jackson2JsonMessageConverter jj = new Jackson2JsonMessageConverter();
+		Object o = jj.fromMessage(message);
 		
-		try {
-			Object o = JSONValue.parseWithException(message.getBody().toString());
-			RequestSerialVO v = (RequestSerialVO) o;
-			System.out.println(v.getId()+"###now it is the ID!");
-		} catch (org.json.simple.parser.ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		RequestSerialVO vo = (RequestSerialVO) o;
+		System.out.println("********now the vo get from mq is:");
+		System.out.println(vo.getId());
+		System.out.println(vo.getPrice());
+		System.out.println(vo.getProjectid());
+		System.out.println(vo.getShares());
+		System.out.println(vo.getUserid());
+		System.out.println("*******end");
 		
 	}
-	public void onMessage(Message message,String i){
-		/*Object obj = amqpTemplate.receiveAndConvert();
-		System.out.println(obj.toString()+"this is from amqptemplate.receiveandconvert()");*/
-		System.out.println("From OnMessage!!");
-		
-		System.out.println("****message get in onmessage*******System.out.println(message.getBody());***");
-		System.out.println(message.getBody());
-		System.out.println("****message get in onmessage*******System.out.println(message.toString());***");
-		System.out.println(message.toString());
-		System.out.println("****message get in onmessage*******System.out.println(message.getMessageProperties());***");
-		System.out.println(message.getMessageProperties());
 
-/*		System.out.println(message.getBody().toString());
-		try {
-			Object o = JSONValue.parseWithException(message.toString());
-			RequestSerialVO v = (RequestSerialVO) o;
-			System.out.println(v.getId()+"is the id!");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		// logger.info("Crowdfundng receive message------->:{}",
-		// message.getBody());
-
-	}
 	public void showVO(RequestSerialVO vo) {
 		System.out.println("********now the vo get from mq is:");
 		System.out.println(vo.getId());
